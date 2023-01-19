d25f0574c2834e0b006c4163209aa55689bbc64e
Sukanya <106397577+Gund008@users.noreply.github.com>
2023-01-19 11:57:08 +0530

    Add files via upload

---
+++ ChkBit4.c
@@ -0,0 +1,47 @@
+//Write a Program which Checks Whether is  7th and 8th and 9th bit is On or Off.
+
+#include <stdio.h>
+
+typedef int BOOL;
+typedef unsigned int UINT;
+
+#define TRUE 1
+#define FALSE 0
+
+BOOL ChkBit(UINT iNo)
+{
+    UINT iMask=0X000001C0;
+    UINT iResult=0;
+
+    iResult=iNo & iMask;
+
+    if(iResult==iMask)
+    {
+        return TRUE;
+    }
+    else
+    {
+       return FALSE;
+    }  
+}
+///////////////////////////////////////////////////////////////////////////////////////////////
+int main()
+{
+    UINT iValue=0;
+    BOOL bRet=FALSE;
+
+    printf("Please Enter Number:\n");
+    scanf("%d",&iValue);
+
+    bRet=ChkBit(iValue);
+    if(bRet==TRUE)
+    {
+        printf("Bit is On");
+    }
+    else
+    {
+        printf("Bit is Off");
+    }
+    return 0;
+}
+
